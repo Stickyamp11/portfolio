0d9d8f56d14a7781d29f707cc3ab2af75abed1af
Stickyamp11 <i82cosem@uco.es>
2022-08-17 20:39:55 +0200

    Fix error

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Stickyamp11/aboutme.git master:gh-pages
+git push -f git@github.com:Stickyamp11/portfolio.git master:gh-pages
 cd -
